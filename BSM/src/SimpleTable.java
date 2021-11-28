c284e865e09c16fbf63d0ae554e46a0ec3392f45
Trong Nguyen <33231414+nguyentrong56@users.noreply.github.com>
2021-11-27 18:57:00 -0800

    changes

--- BSM/src/SimpleTable.java
+++ BSM/src/SimpleTable.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:BSM/src/SimpleTable.java
 import javax.swing.table.DefaultTableModel;
 public class SimpleTable extends DefaultTableModel {
 
@@ -22,29 +21,4 @@ public class SimpleTable extends DefaultTableModel {
         }
 
       
-=======
-import javax.swing.table.DefaultTableModel;
-public class SimpleTable extends DefaultTableModel {
-
-     
-	private static final long serialVersionUID = 1L;
-
-		public void addRow(String[] row) {
-            super.addRow(row);
-            this.fireTableDataChanged();
-     
-        }
-        
-        public void deleteRow(int index) {
-        	super.removeRow(index);
-        	this.fireTableDataChanged();
-        }
-        
-        public void editRow(String[] row, int index) {
-        	super.insertRow(index, row);
-        	this.fireTableDataChanged();
-        }
-
-      
->>>>>>> parent of 3fbf135 (Refactoring layers):BSM/src/View/SimpleTable.java
     }  
